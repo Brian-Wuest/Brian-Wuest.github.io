b61076855e83bcf002c961b173c4ca5055674e9e
WuestMan <redknightofdoom@yahoo.com>
2017-02-03 06:16:35 -0600

    Formatting and various changes
    Added new page title functionality. Added report issue button. Added a
    header to the experience calculator screen.
    
    Updated sizing for larger phones so the collabse button shows up.

--- js/ng-js/app.js
+++ js/ng-js/app.js
@@ -6,20 +6,33 @@
     MyApp.config(function($routeProvider) {
         $routeProvider
             .when("/quick_encounter", {
+                title: "RPG Tools: Quick Encounter",
                 templateUrl: "templates/d_and_d/quick_encounter.html"
             })
             .when("/experience_calculator", {
+                title: "RPG Tools: Experience Calculator",
                 templateUrl: "templates/d_and_d/experience_calculator.html",
                 controller: "ExperienceCalculatorController"
             })
             .when("/minecraft_prefab", {
+                title: "Minecraft Mod: Prefab Showcase",
                 templateUrl: "templates/minecraft/prefab.html"
             })
             .when("/minecraft_wuest_utilities", {
+                title: "Minecraft Mod: WuestUtilities Showcase",
                 templateUrl: "templates/minecraft/wuest_utilities.html"
             })
             .otherwise({
+                title: "Home",
                 templateUrl: "templates/main.html"
             });
     });
+
+    MyApp.run(["$rootScope", "$route", function($rootScope, $route){
+        $rootScope.$on("$routeChangeSuccess", function() {
+            // When the route is successfully changed, pull the title property configired above and place it in the document title.
+            document.title = $route.current.title + " | Brian-Wuest.github.io";
+        });
+    }]);
+
 })();
